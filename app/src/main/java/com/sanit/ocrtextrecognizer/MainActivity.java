d84c7777c87be6f5415c8607e5bb9199e676ce05
sanit7722 <sanit7722@gmail.com>
2020-12-05 18:53:02 +0530

    Added Scan button and text to speech

--- app/src/main/java/com/sanit/ocrtextrecognizer/MainActivity.java
+++ app/src/main/java/com/sanit/ocrtextrecognizer/MainActivity.java
@@ -1,19 +1,136 @@
 package com.sanit.ocrtextrecognizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.speech.tts.TextToSpeech;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
+import static android.Manifest.permission.CAMERA;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+    private TextRecognizer textRecognizer;
+    private String stringResult = null;
+    private TextToSpeech textToSpeech;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, PackageManager.PERMISSION_GRANTED);
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraSource.release();
+    }
+
+    private void setTextRecognizer() {
+        textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer).setRequestedPreviewSize(1200, 1024).build();
+        surfaceView = findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    cameraSource.start(surfaceView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+                cameraSource.stop();
+            }
+        });
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+
+                SparseArray<TextBlock> sparseArray=detections.getDetectedItems();
+                StringBuilder stringBuilder=new StringBuilder();
+                for(int i=1;i<sparseArray.size();++i){
+                    TextBlock textBlock=sparseArray.valueAt(i);
+                    if(textBlock!=null&& textBlock.getValue()!=null){
+                        stringBuilder.append(textBlock.getValue()+" ");
+                    }
+                }
+                final String stringText=stringBuilder.toString();
+                Handler handler=new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        stringResult=stringText;
+                        onResultBack();
+                    }
+                });
+            }
+
+        });
+    }
+
+    private void onResultBack() {
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setText(stringResult);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(stringResult, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
     }
     public void onScanButton(View v){
 
+        setContentView(R.layout.surface_view);
+        setTextRecognizer();
 
     }
 }
